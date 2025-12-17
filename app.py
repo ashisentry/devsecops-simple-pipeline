1a04694d1f4e4836b785846cdfbf09d8f63e4e20
ashisentry <ashisentry@gmail.com>
2025-12-17 08:14:51 +0000

    add new function - version

--- app.py
+++ app.py
@@ -4,3 +4,6 @@ def hello():
 if __name__ == "__main__":
     print(hello())
 
+def version():
+    return "v1.0"
+
--- testapp.py
+++ testapp.py
@@ -3,3 +3,7 @@ from app import hello
 def test_hello():
     assert hello() == "Hello DevSecOps!"
 
+def test_version():
+    from app import version
+    assert version() == "v1.0"
+
